827e392eed724b62268fa015951ccc7c951d6605
alun <garanaw@gmail.com>
2022-06-19 22:03:34 +0100

    Implements must verify email

--- app/Core/User/Infrastructure/Models/User.php
+++ app/Core/User/Infrastructure/Models/User.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Aquelarre\Core\User\Infrastructure\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
