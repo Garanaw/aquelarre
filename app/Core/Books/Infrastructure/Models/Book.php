173c9859aff3055adbe657118207f974db7b7d3c
alun <garanaw@gmail.com>
2022-06-26 16:56:39 +0100

    Adds docblocks

--- app/Core/Books/Infrastructure/Models/Book.php
+++ app/Core/Books/Infrastructure/Models/Book.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     /** @var array<string, mixed> */
+    // phpcs:ignore SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint -- baseline
     protected $casts = [
         'id' => 'int',
     ];
