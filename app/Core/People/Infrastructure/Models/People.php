4b9fbc7e039d83b48cf0ce8f86723be57fd20cd5
alun <garanaw@gmail.com>
2022-07-16 17:26:45 +0100

    Adds people

---
+++ app/Core/People/Infrastructure/Models/People.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aquelarre\Core\People\Infrastructure\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class People extends Model
+{
+    use HasFactory;
+
+    protected $table = 'people';
+}
