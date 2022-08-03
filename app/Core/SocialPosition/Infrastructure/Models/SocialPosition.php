6b61545a65e189acd98ee361ed4a7aeddae651ab
alun <garanaw@gmail.com>
2022-08-03 19:48:54 +0100

    Adds expenses model

--- app/Core/SocialPosition/Infrastructure/Models/SocialPosition.php
+++ app/Core/SocialPosition/Infrastructure/Models/SocialPosition.php
@@ -6,8 +6,14 @@ namespace Aquelarre\Core\SocialPosition\Infrastructure\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class SocialPosition extends Model
 {
     use HasFactory;
+
+    public function expenses(): HasOne
+    {
+        return $this->hasOne(related: Expense::class);
+    }
 }
