e3c5e5fd83cc5bb1041f2767a8aa1b14d5742600
alun <garanaw@gmail.com>
2022-08-13 20:50:28 +0100

    Removes debug lines

--- app/Static/Application/routes.php
+++ app/Static/Application/routes.php
@@ -7,6 +7,4 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 $router = app(Router::class);
 
-Aquelarre\Debug::$debug = true;
 $router->view('/', 'static::welcome')->name('home');
-Aquelarre\Debug::$debug = false;
