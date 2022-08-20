302ebff6f249b1f0c45765075e42a67be32a43e8
alun <garanaw@gmail.com>
2022-08-20 17:22:33 +0100

    Adds favicon

--- app/Static/Presentation/Resources/views/welcome.blade.php
+++ app/Static/Presentation/Resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/static/favicon.ico') }}">
 
         <title>Laravel</title>
 
