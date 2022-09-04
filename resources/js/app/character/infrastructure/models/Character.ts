ee780c9cf94f5965df5f99ea8d51c37cf77260a3
alun <garanaw@gmail.com>
2022-09-04 15:00:04 +0100

    Adds doc-blocks

--- resources/js/app/character/infrastructure/models/Character.ts
+++ resources/js/app/character/infrastructure/models/Character.ts
@@ -1,6 +1,10 @@
 import Model from '../../../../framework/database/eloquent/Model';
 import Eloquent from "../../../../framework/database/eloquent/Eloquent";
 
+/**
+ * @property {number} id
+ * @property {string} name
+ */
 // @ts-ignore
 @Eloquent
 export default class Character extends Model {
