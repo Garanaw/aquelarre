aff418433c53a8a7a30caaf54f6746bac69d4faf
alun <garanaw@gmail.com>
2022-09-21 22:34:34 +0100

    phpcs

--- app/Core/Kingdom/Infrastructure/Models/Kingdom.php
+++ app/Core/Kingdom/Infrastructure/Models/Kingdom.php
@@ -31,7 +31,7 @@ class Kingdom extends Model
         'description' => AsStringable::class,
     ];
 
-    // phpcs:ignore SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
+    // phpcs:ignore SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
     public function newEloquentBuilder($query): KingdomBuilder
     {
         return new KingdomBuilder($query);
