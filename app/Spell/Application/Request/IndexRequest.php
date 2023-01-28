7c864814c7bd9cb89db7da10da547c662cd363a8
alun <garanaw@gmail.com>
2023-01-28 22:24:05 +0000

    Removes unused contract

--- app/Spell/Application/Request/IndexRequest.php
+++ app/Spell/Application/Request/IndexRequest.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace Aquelarre\Spell\Application\Request;
 
 use Aquelarre\Core\Framework\Application\Request\Searchable;
-use Aquelarre\Core\Framework\Domain\Concerns\EncodesIds;
-use Aquelarre\Core\Framework\Domain\Contracts\HasDecodableValues;
 use Aquelarre\Core\Framework\Domain\Search\Searcher;
 use Aquelarre\Spell\Domain\Searchers\IndexSearch;
 use Illuminate\Foundation\Http\FormRequest;
 
-class IndexRequest extends FormRequest implements Searchable, HasDecodableValues
+class IndexRequest extends FormRequest implements Searchable
 {
-    use EncodesIds;
-
     public function rules(): array
     {
         return [
@@ -43,26 +39,6 @@ class IndexRequest extends FormRequest implements Searchable, HasDecodableValues
                 'integer',
             ],
         ];
-    }
-
-    public function getDecodableKeys(): array
-    {
-        return [
-            'form',
-            'origin',
-            'nature',
-            'book',
-        ];
-    }
-
-    public function decodeValues(array $values): array
-    {
-        return collect($this->validated())
-            ->map(
-                fn (mixed $value, string $key): mixed => $value && in_array($key, $this->getDecodableKeys())
-                    ? $this->decode((int)$value)
-                    : $value
-            )->toArray();
     }
 
     public function getSearcher(): Searcher
