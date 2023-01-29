1784fcd084ddde020411dbe945e2f16cba41e2f1
alun <garanaw@gmail.com>
2023-01-29 00:14:21 +0000

    phpcs

--- app/Spell/Infrastructure/Models/SpellNature.php
+++ app/Spell/Infrastructure/Models/SpellNature.php
@@ -17,6 +17,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  */
 class SpellNature extends Model
 {
+    /** @var string[] */
     protected $casts = [
         'name' => Stringable::class,
         'description' => Stringable::class,
