5dc09e28f3f4b294bef77b4c7dc6c32c96b668e1
alun <garanaw@gmail.com>
2023-01-29 00:45:08 +0000

    adds clockwork

--- config/debugbar.php
+++ config/debugbar.php
@@ -137,7 +137,7 @@ return [
      | Extension, without the server-side code. It uses Debugbar collectors instead.
      |
      */
-    'clockwork' => false,
+    'clockwork' => true,
 
     /*
      |--------------------------------------------------------------------------
