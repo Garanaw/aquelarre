0ad780c3fa05e7a3a6721039bee1da21a2ee8db5
alun <garanaw@gmail.com>
2023-05-28 20:25:14 +0100

    Deletes unused model

--- resources/js/framework/database/eloquent/domain/Exception/ModelException.ts
+++ resources/js/framework/database/eloquent/domain/Exception/ModelException.ts
@@ -1,20 +1,19 @@
 import Exception from "../../../../Exceptions/Exception";
-import Model from "../../Model";
 
 export default class ModelException extends Exception
 {
-    private model: Model<any> | null = null;
+    private model: any | null = null;
 
-    constructor(message: string, code: number = 500, model: Model<any> | null = null) {
+    constructor(message: string, code: number = 500, model: any | null = null) {
         super(message, code);
         this.model = model;
     }
 
-    getModel(): Model<any> | null {
+    getModel(): any | null {
         return this.model;
     }
 
-    setModel(model: Model<any> | null): this {
+    setModel(model: any | null): this {
         this.model = model;
         return this;
     }
