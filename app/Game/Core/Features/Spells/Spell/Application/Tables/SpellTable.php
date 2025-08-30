4368cf4a057487d2588a769eaf99f3af8b8bd64f
Garanaw <garanaw@gmail.com>
2025-08-30 12:36:59 +0200

    Adds filters to spells

--- app/Game/Core/Features/Spells/Spell/Application/Tables/SpellTable.php
+++ app/Game/Core/Features/Spells/Spell/Application/Tables/SpellTable.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Game\Core\Features\Spells\Spell\Application\Tables;
 
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class SpellTable
@@ -44,6 +45,32 @@ class SpellTable
                     ->label('Source Book')
                     ->searchable()
                     ->sortable(),
+            ])->filters([
+                SelectFilter::make('vis')
+                    ->label('Vis')
+                    ->options([
+                        1 => '1',
+                        2 => '2',
+                        3 => '3',
+                        4 => '4',
+                        5 => '5',
+                        6 => '6',
+                        7 => '7',
+                    ])->multiple(),
+                SelectFilter::make('form')
+                    ->label('Form')
+                    ->relationship('form', 'name')
+                    ->multiple(),
+                SelectFilter::make('nature')
+                    ->label('Nature')
+                    ->relationship('nature', 'name'),
+                SelectFilter::make('origin')
+                    ->label('Origin')
+                    ->relationship('origin', 'name')
+                    ->multiple(),
+                SelectFilter::make('book')
+                    ->label('Source Book')
+                    ->relationship('book', 'name')
             ]);
     }
 }
