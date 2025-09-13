3b3a4eea144fdbdd833ae1854e19488910ef6ccf
Garanaw <garanaw@gmail.com>
2025-09-13 16:07:25 +0200

    Adds ability to disable D10 button

--- resources/js/app/components/button/D10Button.vue
+++ resources/js/app/components/button/D10Button.vue
@@ -1,12 +1,26 @@
 <template>
-    <Button asChild v-slot="slotProps" severity="info">
-        <D10 @roll:d10="$emit('roll:d10')" />
+    <Button v-slot="slotProps" severity="info" :disabled="disabled" raised>
+        <D10 @roll:d10="rollD10" />
     </Button>
 </template>
 
 <script setup lang="ts">
 import Button from 'primevue/button';
 import D10 from "../dice/D10.vue";
+import {computed} from "vue";
 
-defineEmits(['roll:d10']);
+const props = defineProps<{
+    disabled?: boolean
+}>();
+
+const disabled = computed(() => props.disabled ?? false);
+const emit = defineEmits(['roll:d10']);
+
+function rollD10() {
+    if (props.disabled) {
+        return;
+    }
+
+    emit('roll:d10');
+}
 </script>
